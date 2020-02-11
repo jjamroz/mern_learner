2da72d0e4b4b0cb3d584b8c19231124e28343e7c
Jamroz <jakub.jamroz@capgemini.com>
2020-02-11 17:48:31 +0100

    sets routes added - only update missing

--- routes/sets.js
+++ routes/sets.js
@@ -52,4 +52,96 @@ router.post(
   }
 );
 
+// @route   GET api/sets/
+// @desc    Get all user sets name list
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const sets = await Set.find({ user: req.user.id })
+      .sort({
+        date: -1
+      })
+      .select('name');
+    res.json(sets);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/sets/public
+// @desc    Get all public set names
+// @access  Public
+router.get('/public/', async (req, res) => {
+  try {
+    const sets = await Set.find({ category: 'public' })
+      .sort({
+        date: -1
+      })
+      .select('name');
+    res.json(sets);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/sets/{id}
+// @desc    Get set
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const set = await Set.findById(req.params.id);
+    if (!set) {
+      return res.status(404).json({ msg: 'Set not found' });
+    }
+    if (set.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+    res.json(set);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/sets/public/{id}
+// @desc    Get set
+// @access  Public
+router.get('/public/:id', async (req, res) => {
+  try {
+    const set = await Set.findById(req.params.id);
+    if (!set) {
+      return res.status(404).json({ msg: 'Set not found' });
+    }
+    if (set.category !== 'public') {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+    res.json(set);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   DELETE api/sets/
+// @desc    delete set
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const set = await Set.findById(req.params.id);
+    if (!set) {
+      return res.status(404).json({ msg: 'Set not found' });
+    }
+    if (set.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+    await Set.findByIdAndRemove(req.params.id);
+    res.json({ msg: 'Set removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
